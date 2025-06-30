fd1d6fc83c6e91cf019cf69cf22edc04387022ec
partykyoung <partykyoung@gmail.com>
2025-07-01 03:50:00 +0900

    언어 토글 기능 추가
    - 영어와 한국어 간의 언어 전환 기능 구현
    - 언어 변경 버튼 추가 및 스타일 적용
    - 다국어 지원을 위한 i18n 설정 추가
    - 즐겨찾기 및 DApp 목록 페이지에 언어 토글 컴포넌트 통합

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,10 +1,12 @@
 import { BannerList } from "./bannder-list";
 import { DappList } from "./dapp-list";
 import { FavoritesList } from "./favorites-list";
+import { LanguageToggle } from "./language-toggle";
 
 function MainPage() {
   return (
     <div className="w-full max-w-md mx-auto">
+      <LanguageToggle />
       <BannerList />
       <FavoritesList />
       <DappList />
